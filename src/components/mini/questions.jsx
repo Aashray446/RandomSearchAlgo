2340ce3edb492d679696b63fc9b10c435f80ee9b
Aashray Katiyar <aashray446@gmail.com>
2023-01-05 22:15:21 +0530

    <Feat> : [Added Multiplte question functionality]

--- src/components/mini/questions.jsx
+++ src/components/mini/questions.jsx
@@ -1,27 +1,24 @@
 import React from 'react';
 
-export default function Questions() {
+export default function Questions(props) {
 
-    const [checked, setChecked] = React.useState({
-        value: ""
-    });
+    const Questions = props.question
 
     return (
         <div className="form-control mb-4 ">
-            <label className="block mb-5">Q1. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque repellendus ea nostrum necessitatibus similique explicabo molestias sapiente rem neque! Ea.</label>
+            <label className="block font-medium font-sans mb-5">{Questions.question}</label>
             <div className="flex flex-row flex-wrap gap-5 justify-self-stretch">
-                <div className='flex flex-row'>
-                    <input type="radio" name="radio-2" className="radio label-text radio-primary inline-block" />
-                    <span> Lorem ipsum dolor sit amet.</span>
-                </div>
-                <div className='flex flex-row'>
-                    <input type="radio" name="radio-2" className="radio label-text radio-primary inline-block" />
-                    <span> Lorem, ipsum do</span>
-                </div>
-                <div className='flex flex-row'>
-                    <input type="radio" name="radio-2" className="radio label-text radio-primary inline-block" />
-                    <span> sit amet.</span>
-                </div>
+                {
+                    Questions.options.map((option, index) => {
+                        return (
+                            <div className='flex flex-row' key={index}>
+                                <input type="radio" name={props.id + 'r'} value={option.value} className="radio radio-primary inline-block" />
+                                <span className='font-light'> {option.option} </span>
+                            </div>
+                        )
+                    }
+                    )
+                }
             </div>
             <hr className='m-4' />
         </div>
