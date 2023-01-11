7508d1b3cd140b59a8c121d316cfd3820abe8791
Aashray Katiyar <aashray446@gmail.com>
2023-01-12 00:10:11 +0530

    <Feat> : [Added Input fields for searching]

--- src/components/controllerPanel.jsx
+++ src/components/controllerPanel.jsx
@@ -1,7 +1,19 @@
 import Switch from "./mini/switch";
 import { randomSearch } from "../experiments/randomSearch";
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 export default function ControllerPanel({ changeGraph, nodesAndLinks }) {
+
+    const [source, setSource] = useState('');
+    const [destination, setDestination] = useState('');
+
+    const handleSourceChange = (e) => {
+        setSource(e.target.value);
+    }
+
+    const handleDestinationChange = (e) => {
+        setDestination(e.target.value);
+    }
+
 
     useEffect(() => {
         // console.log(nodesAndLinks);
@@ -11,9 +23,9 @@ export default function ControllerPanel({ changeGraph, nodesAndLinks }) {
         <div className="grid grid-cols-2 gap-10 tracking-widest p-5 bg-base-100 m-4">
             <Switch id={"toggle"} ></Switch>
             <button className="btn btn-primary" onClick={() => changeGraph(10, 15)} >Generate Random</button>
-            <input type="number" className="input input-bordered input-accent bg-white" placeholder="Source Node" />
-            <input type="number" className="input input-bordered input-accent bg-white col-span-full " placeholder="Destination Node" />
-            <button className="btn btn-secondary" onClick={() => randomSearch(nodesAndLinks.nodes, nodesAndLinks.links, nodesAndLinks.nodes[0].id, nodesAndLinks.nodes[5].id)}>Find Path</button>
+            <input type="text" value={source} onChange={handleSourceChange} className="input input-bordered input-accent bg-white" placeholder="Source Node" />
+            <input type="text" value={destination} onChange={handleDestinationChange} className="input input-bordered input-accent bg-white col-span-full " placeholder="Destination Node" />
+            <button className="btn btn-secondary" onClick={() => randomSearch(nodesAndLinks.nodes, nodesAndLinks.links, source, destination)}>Find Path</button>
         </div>
 
     );
